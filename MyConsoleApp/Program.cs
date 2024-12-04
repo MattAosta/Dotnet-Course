3fcd2be31a5f0fafbcd51c619c2b6c981d566a07
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-12-04 16:07:07 +0900

    Concatenating strings

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,9 +1,3 @@
-﻿Console.WriteLine("Hello\nWorld!");//escaping new line
-Console.WriteLine("Hello\tWorld!");//escaping tab
-Console.WriteLine("Hello \"World\"!");//escaping double quote
-Console.WriteLine("c:\\source\\repos");//escaping slash
-Console.WriteLine(@"    c:\source\repos    
-        (this is where your code goes)");//escaping Verbatim string literal
-
-// Kon'nichiwa World
-Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");//Unicode escape characters
+﻿string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
