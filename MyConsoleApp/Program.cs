04ca485f8b1f63fb136e37ee56d767e6e3057eb4
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-12-04 15:42:50 +0900

    Variables declaration, initialization and errors

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,5 +1,18 @@
-﻿//Variable declaration
-char userOption;
-int gameScore;
-decimal particlesPerMillion;
-bool processedCustomer;
+﻿string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+//Errors:
+//firstName = 123//causes error
+
+//string firstName;
+//Console.WriteLine(firstName);
+
+string secondName = "Bob";
+Console.WriteLine(secondName);
