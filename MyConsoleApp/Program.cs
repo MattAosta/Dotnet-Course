263e564f98e559ba9ce2c151147464ebe65523f6
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-12-04 09:27:39 +0900

    Comments and difference between Write and WriteLine

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+﻿//Console.WriteLine('Hello World!');
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
