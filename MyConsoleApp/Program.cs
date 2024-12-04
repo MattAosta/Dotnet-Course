624ae8cffa679f6c7336b5a670ab572f0cc22a65
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-12-04 15:50:29 +0900

    Second exercise, Variables and Data Types

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,4 +1,11 @@
-﻿var message = "Hello world!";
+﻿string name = "Bob";
+int messages = 3;
+float temperature = 34.4f;
 
-//Errors:
-//var message;
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! you have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
