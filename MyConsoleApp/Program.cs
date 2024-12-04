12717e1c062286a16fc80df6b2f69d5b971ad0d2
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-12-04 15:45:26 +0900

    Implicitly typed local variables

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,18 +1,4 @@
-﻿string firstName;
-firstName = "Bob";
-Console.WriteLine(firstName);
-firstName = "Liem";
-Console.WriteLine(firstName);
-firstName = "Isabella";
-Console.WriteLine(firstName);
-firstName = "Yasmin";
-Console.WriteLine(firstName);
+﻿var message = "Hello world!";
 
 //Errors:
-//firstName = 123//causes error
-
-//string firstName;
-//Console.WriteLine(firstName);
-
-string secondName = "Bob";
-Console.WriteLine(secondName);
+//var message;
