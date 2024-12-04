f9816816fa04be9eef223872a9b0737918f36abe
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-12-04 15:39:14 +0900

    Variable declaration

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,22 +1,5 @@
-﻿//Character Literal
-//Only 1 character
-Console.WriteLine('b');
-
-//Integer Literal
-Console.WriteLine(123);
-
-//Floating-point Literal
-//literal suffic can be both f and F
-//least precise, best for fixed fractional values
-Console.WriteLine(0.25F);
-
-//Double Literal (default for decimals)
-Console.WriteLine(2.625);
-
-//Decimal Literal
-//literal suffix can be both m or M
-Console.WriteLine(12.39816m);
-
-//Bolean Literal
-Console.WriteLine(true);
-Console.WriteLine(false);
+﻿//Variable declaration
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
