464547ea3ce870b791f99400875f5c6bb7c6dac1
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-12-13 12:47:31 +0900

    Interpolation with String, Int and Verbatim Literals

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,3 +1,11 @@
 ﻿string firstName = "Bob";
 string greeting = "Hello";
-Console.WriteLine(greeting + " " + firstName + "!");
+string message = $"{greeting} {firstName}!";
+Console.WriteLine(message);
+
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}");
+
+string projectName = "First-project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
