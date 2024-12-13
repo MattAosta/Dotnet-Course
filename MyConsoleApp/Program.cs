56c98a97ceb26681c022cb1941768ef0f083acee
Matteo Marguerettaz <matteo.marguerettaz@gmail.com>
2024-12-13 13:09:20 +0900

    Challenge with Verbatim and Interpolation

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,11 +1,10 @@
-﻿string firstName = "Bob";
-string greeting = "Hello";
-string message = $"{greeting} {firstName}!";
-Console.WriteLine(message);
+﻿string projectName = "ACME";
+string path = @"c:\Exercise\";
+string englishFolder = "";
+string russianFolder = @"ru-RU\";
+string englishMessage = "View English output";
+string file = "data.txt";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
-int version = 11;
-string updateText = "Update to Windows";
-Console.WriteLine($"{updateText} {version}");
-
-string projectName = "First-project";
-Console.WriteLine($@"C:\Output\{projectName}\Data");
+Console.WriteLine($"{englishMessage}:\n\t\t{path}{englishFolder}{projectName}\\{file}\n");
+Console.WriteLine($"{russianMessage}:\n\t\t{path}{russianFolder}{projectName}\\{file}\n");
